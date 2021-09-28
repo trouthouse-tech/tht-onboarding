b193baef81e59838aa3abf512ab38137481d0bc0
Cameron Romo <cromo1225@gmail.com>
2021-09-28 14:45:14 -0600

    Added logo and non-functional buttons

--- App.tsx
+++ App.tsx
@@ -1,15 +1,55 @@
 import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Text>Hello World!</Text>
+    <SafeAreaView style={styles.safeAreaView}>
+      <Image source={require('./assets/img/logo.png')} style={styles.image} />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={[styles.createAccountButton, styles.button]}>
+          <Text>Create Account</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.signInButton, styles.button]}>
+          <Text>Sign In</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  button: {
+    width: 200,
+    height: 50,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  createAccountButton: {
+    borderWidth: 0.5,
+    borderColor: '#FF7C1E',
+  },
+  image: {
+    flex: 1,
+    height: 190,
+    width: 140,
+    resizeMode: 'contain',
+  },
+  safeAreaView: {
+    flex: 1,
+    backgroundColor: '#FFFBF9',
+    alignItems: 'center',
+  },
+  signInButton: {
+    backgroundColor: '#FF7C1E',
+    marginTop: 10,
+  },
+});
 
 export default App;
