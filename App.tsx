a70b0ab1446711798c5393e5620df3923c45fde0
AJ <alexanderjessediaz@gmail.com>
2021-09-30 14:38:41 -0600

    bring in view to wrap buttons, finish styling TO buttons

--- App.tsx
+++ App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import {Image, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <Image source={require('../tht-onboarding/assets/img/logo.png')} style={styles.image} />
-      <TouchableOpacity>
-        <Text>Create Account</Text>
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <Text>Sign In</Text>
-      </TouchableOpacity>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.createAccBtn}>
+          <Text>Create Account</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.signInBtn}>
+          <Text>Sign In</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
@@ -21,9 +23,34 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFFBF9',
     alignItems: 'center',
   },
+  buttonContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  createAccBtn: {
+    borderWidth: 0.5,
+    borderColor: '#FF7C1E',
+    height: 50,
+    width: 200,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  signInBtn: {
+    backgroundColor: '#FF7C1E',
+    height: 50,
+    width: 200,
+    marginTop: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   image: {
+    flex: 1,
     height: 190,
     width: 140,
+    resizeMode: 'contain',
   },
 });
 
