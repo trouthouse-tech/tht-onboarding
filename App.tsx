dd67cb5e5c8392f9c2c41abbe4448f5298868f2b
AJ <alexanderjessediaz@gmail.com>
2021-09-30 14:06:14 -0600

    import logo img from assets into app.tsx, set height/width on image style

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import {Image, StyleSheet, Text} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>Hello World</Text>
+      <Image source={require('../tht-onboarding/assets/img/logo.png')} style={styles.image} />
     </SafeAreaView>
   );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 190,
+    width: 140,
+  },
+});
 
 export default App;
