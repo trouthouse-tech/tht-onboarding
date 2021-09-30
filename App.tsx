3c272e12d037fe90636a73ea359f40ed8d8a5c1e
AJ <alexanderjessediaz@gmail.com>
2021-09-30 14:22:32 -0600

    import touchableopacity, center logo with correct background color, add 2 TO's

--- App.tsx
+++ App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import {Image, StyleSheet, Text} from 'react-native';
+import {Image, StyleSheet, Text, TouchableOpacity} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.safeAreaView}>
       <Image source={require('../tht-onboarding/assets/img/logo.png')} style={styles.image} />
+      <TouchableOpacity>
+        <Text>Create Account</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text>Sign In</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
 const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+    backgroundColor: '#FFFBF9',
+    alignItems: 'center',
+  },
   image: {
     height: 190,
     width: 140,
